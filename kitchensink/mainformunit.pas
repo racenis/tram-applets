735e96e2f0a736de0c48fd85c5beb1c714188210
racenis <martinsrasaolaslasa@gmail.com>
2024-11-27 14:04:06 +0200

    Started on kitchensink editor

---
+++ kitchensink/mainformunit.pas
@@ -0,0 +1,119 @@
+unit MainFormUnit;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, Menus,
+  StdCtrls, ExtCtrls, Spin;
+
+type
+
+  { TMainForm }
+
+  TMainForm = class(TForm)
+    ItemTabNewItem: TButton;
+    ItemTabDeleteItem: TButton;
+    ItemTabNewAttribute: TButton;
+    ItemTabDeleteAttribute: TButton;
+    ItemTabBaseClass: TComboBox;
+    ItemTabItemCompartment: TComboBox;
+    ItemTabViewmodel: TComboBox;
+    ItemTabWorldmodel: TComboBox;
+    ItemTabEquipmentSlot: TComboBox;
+    ItemTabSprite: TComboBox;
+    ItemTabIcon: TComboBox;
+    ItemTabFile: TComboBox;
+    ItemTabAttribute: TComboBox;
+    ItemTabEffectType: TComboBox;
+    ItemTabSearchItem: TEdit;
+    ItemTabClassName: TEdit;
+    ItemTabEffectTag: TEdit;
+    ItemTabEffectName: TEdit;
+    ItemTabAttributeValue: TFloatSpinEdit;
+    ItemTabEffectDuration: TFloatSpinEdit;
+    ItemTabItemWeight: TFloatSpinEdit;
+    GroupBox1: TGroupBox;
+    ItemTabLayoutGroup: TGroupBox;
+    ItemTabGeneralGroup: TGroupBox;
+    ItemTab2DGroup: TGroupBox;
+    ItemTab3DGroup: TGroupBox;
+    Label1: TLabel;
+    Label10: TLabel;
+    Label11: TLabel;
+    Label12: TLabel;
+    Label13: TLabel;
+    Label14: TLabel;
+    Label15: TLabel;
+    Label16: TLabel;
+    Label17: TLabel;
+    Label18: TLabel;
+    Label19: TLabel;
+    Label2: TLabel;
+    Label20: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
+    ItemTabItemList: TListBox;
+    ItemTabAttributeList: TListBox;
+    MainMenu: TMainMenu;
+    MenuItemAbout: TMenuItem;
+    MenuItemHelp: TMenuItem;
+    MenuItemGetHelp: TMenuItem;
+    MenuItemQuit: TMenuItem;
+    MenuItemNewCharacter: TMenuItem;
+    MenuItemNewItem: TMenuItem;
+    MenuItemNewDialog: TMenuItem;
+    MenuItemNewQuest: TMenuItem;
+    MenuItemFile: TMenuItem;
+    ItemTabAttributeRadio: TRadioButton;
+    ItemTabEffectRadio: TRadioButton;
+    ItemTabAttributeEffectSwitch: TRadioGroup;
+    ItemTabSpriteFrame: TSpinEdit;
+    ItemTabIconFrame: TSpinEdit;
+    ItemTabItemWidth: TSpinEdit;
+    ItemTabItemHeight: TSpinEdit;
+    ItemTabItemStack: TSpinEdit;
+    MenuItemLoadDatabase: TMenuItem;
+    MenuItemSaveFiles: TMenuItem;
+    Separator1: TMenuItem;
+    Separator2: TMenuItem;
+    Separator3: TMenuItem;
+    Tabs: TPageControl;
+    GeneralTab: TTabSheet;
+    ItemTab: TTabSheet;
+    DropTableTab: TTabSheet;
+    CharacterTab: TTabSheet;
+    ClassTab: TTabSheet;
+    FactionTab: TTabSheet;
+    AIPackageTab: TTabSheet;
+    QuestTab: TTabSheet;
+    DialogTab: TTabSheet;
+    procedure ItemTabDeleteAttributeClick(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.lfm}
+
+{ TMainForm }
+
+procedure TMainForm.ItemTabDeleteAttributeClick(Sender: TObject);
+begin
+
+end;
+
+end.
+
